8e9c7a94ee427ea0dc7bae362b61d5aa25ddc813
nishant <nishant.singh87@gmail.com>
2020-06-06 16:29:38 +0530

    Script to run cucumber.

--- src/setup-store.ts
+++ src/setup-store.ts
@@ -4,6 +4,10 @@ const redisDriver = process.env.FAKE_REDIS ? require('fakeredis') : require('red
 
 setDriver(redisDriver);
 
+if(process.env.FAKE_REDIS){
+    console.log("Using fake redis");
+}
+
 (async() => {
   await store.setObject("key-1", {key: 'key-1'})
   const value = await store.getObject('key-1');
